6a78f96456131bd0a86d4ab931b2be18ab1f1129
KotlinNLP Authors <github@kotlinnlp.com>
2019-12-20 19:05:10 +0100

    Fix MovingAverage

--- src/main/kotlin/com/kotlinnlp/utils/stats/MovingAverage.kt
+++ src/main/kotlin/com/kotlinnlp/utils/stats/MovingAverage.kt
@@ -8,37 +8,51 @@
 package com.kotlinnlp.utils.stats
 
 /**
- * Convenient class to compute the moving average, giving more importance to the recent added values.
+ * Compute the moving average considering only the last [windowSize] values added.
+ *
+ * @param windowSize the size of the observation window
  */
-class MovingAverage {
+class MovingAverage(private val windowSize: Int) {
 
   /**
-   * The mean.
+   * The values collected within the observation window.
    */
-  var mean: Double = 0.0
-    private set
+  private val values: MutableList<Double> = mutableListOf()
 
   /**
-   * The variance.
-   */
-  var variance: Double = 0.0
-    private set
-
-  /**
-   * Counts the added values.
-   */
-  var count: Long = 0
-    private set
-
-  /**
-   * Add the given [value] to the moving average
+   * Add a value to the moving average.
    *
-   * @param value the value to add to the moving average
+   * @param value a new value
    */
   fun add(value: Double) {
 
-    this.count++
-    this.mean += (2.0 / this.count) * (value - this.mean)
-    this.variance += (2.0 / this.count) * ((value - this.mean) * (value - this.mean) - this.variance)
+    this.values.add(value)
+
+    if (this.values.size > this.windowSize) this.values.removeAt(0)
   }
+
+  /**
+   * @return the mean of the values in the observation window
+   */
+  fun calcMean(): Double = this.values.average()
+
+  /**
+   * @return the variance of the values in the observation window
+   */
+  fun calcVar(): Double {
+
+    val mean: Double = this.calcMean()
+
+    return this.values.asSequence()
+      .map {
+        val diff: Double = it - mean
+        diff * diff
+      }
+      .average()
+  }
+
+  /**
+   * @return the standard deviation of the values in the observation window
+   */
+  fun calcStdDev(): Double = Math.sqrt(this.calcVar())
 }
